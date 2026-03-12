2249a32ab0bcaead8237f59b887174844c230cee
ZfT2 <boener@freenet.de>
2026-03-12 12:07:13 +0100

    fixes

--- src/main/java/de/gbanking/gui/fx/util/TableColumnFactory.java
+++ src/main/java/de/gbanking/gui/fx/util/TableColumnFactory.java
@@ -1,11 +1,11 @@
 package de.gbanking.gui.fx.util;
 
-import javafx.beans.property.SimpleStringProperty;
-import javafx.scene.control.TableColumn;
-
 import java.util.Calendar;
 import java.util.Date;
 import java.util.function.Function;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.scene.control.TableColumn;
 
 public final class TableColumnFactory {
 
@@ -13,7 +13,6 @@ public final class TableColumnFactory {
 	}
 
 	public static <S> TableColumn<S, String> createTextColumn(String title, Function<S, String> valueProvider, double minWidth, double prefWidth) {
-
 		TableColumn<S, String> column = new TableColumn<>(title);
 		column.setCellValueFactory(data -> new SimpleStringProperty(safe(valueProvider.apply(data.getValue()))));
 		FxTableUtils.setPreferredWidth(column, minWidth, prefWidth);
@@ -21,7 +20,6 @@ public final class TableColumnFactory {
 	}
 
 	public static <S> TableColumn<S, String> createFixedTextColumn(String title, Function<S, String> valueProvider, double width) {
-
 		TableColumn<S, String> column = new TableColumn<>(title);
 		column.setCellValueFactory(data -> new SimpleStringProperty(safe(valueProvider.apply(data.getValue()))));
 		FxTableUtils.setFixedWidth(column, width);
@@ -29,21 +27,18 @@ public final class TableColumnFactory {
 	}
 
 	public static <S> TableColumn<S, String> createWrappedTextColumn(String title, Function<S, String> valueProvider, double minWidth, double prefWidth) {
-
 		TableColumn<S, String> column = createTextColumn(title, valueProvider, minWidth, prefWidth);
 		column.setCellFactory(FxTableUtils.createWrappedTextCellFactory());
 		return column;
 	}
 
 	public static <S> TableColumn<S, String> createAmountColumn(String title, Function<S, String> valueProvider, double width) {
-
 		TableColumn<S, String> column = createFixedTextColumn(title, valueProvider, width);
 		column.setCellFactory(FxTableUtils.createAmountCellFactory());
 		return column;
 	}
 
 	public static <S> TableColumn<S, String> createDateColumn(String title, Function<S, Date> valueProvider, double width) {
-
 		TableColumn<S, String> column = new TableColumn<>(title);
 		column.setCellValueFactory(data -> new SimpleStringProperty(DateFormatUtils.formatShort(valueProvider.apply(data.getValue()))));
 		FxTableUtils.setFixedWidth(column, width);
@@ -51,7 +46,6 @@ public final class TableColumnFactory {
 	}
 
 	public static <S> TableColumn<S, String> createCalendarDateColumn(String title, Function<S, Calendar> valueProvider, double width) {
-
 		TableColumn<S, String> column = new TableColumn<>(title);
 		column.setCellValueFactory(data -> new SimpleStringProperty(DateFormatUtils.formatShort(valueProvider.apply(data.getValue()))));
 		FxTableUtils.setFixedWidth(column, width);
@@ -67,7 +61,6 @@ public final class TableColumnFactory {
 	}
 
 	public static <S> TableColumn<S, String> createBooleanAsTextColumn(String title, Function<S, Boolean> valueProvider, double width) {
-
 		TableColumn<S, String> column = new TableColumn<>(title);
 		column.setCellValueFactory(data -> new SimpleStringProperty(Boolean.TRUE.equals(valueProvider.apply(data.getValue())) ? "true" : "false"));
 		FxTableUtils.setFixedWidth(column, width);
