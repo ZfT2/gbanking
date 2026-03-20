43d3d2a77c599de2bdd2207b1823d2f6d1eda908
ZfT2 <boener@freenet.de>
2026-03-20 18:53:25 +0100

    messages

--- src/main/java/de/gbanking/messages/Messages.java
+++ src/main/java/de/gbanking/messages/Messages.java
@@ -4,8 +4,13 @@ import java.text.MessageFormat;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Messages {
 	
+	private static Logger log = LogManager.getLogger(Messages.class);
+
 	static Messages messages;
 
 	private static ResourceBundle messageBundle;
@@ -27,15 +32,24 @@ public class Messages {
 	}
 	
 	public String getFormattedMessage(String key, String param) {
-		return MessageFormat.format(messageBundle.getString(key), param);
+		return getFormatted(key, param);
 	}
 	
 	public String getFormattedMessage(String key, int param) {
-		return MessageFormat.format(messageBundle.getString(key), param);
+		return getFormatted(key, param);
 	}
 	
 	public String getFormattedMessage(String key, Object[] params) {
-		return MessageFormat.format(messageBundle.getString(key), params);
+		return getFormatted(key, params);
 	}
 	
+	private String getFormatted(String key, Object... params) {
+		try {
+			return MessageFormat.format(messageBundle.getString(key), params);
+		} catch (Exception e) {
+			log.error("could not create message for key: {} : {}", key, params);
+		}
+		return "<unknown: " + key + ">";
+	}
+
 }
