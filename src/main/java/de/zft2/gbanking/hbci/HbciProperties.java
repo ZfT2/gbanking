fefb6bf54213d8f7c3bb5c6f3902a09d2fe0b473
ZfT2 <commit@gbanking.net>
2026-08-25 11:30:32 +0200

    fix: Set product version in FinTS messages

--- src/main/java/de/zft2/gbanking/hbci/HbciProperties.java
+++ src/main/java/de/zft2/gbanking/hbci/HbciProperties.java
@@ -2,6 +2,7 @@ package de.zft2.gbanking.hbci;
 
 import java.util.Properties;
 
+import de.zft2.gbanking.db.BuildInfo;
 import de.zft2.gbanking.logging.LoggingSettings;
 
 public final class HbciProperties {
@@ -9,6 +10,8 @@ public final class HbciProperties {
 	public static final String LOG_LEVEL_PARAM = "log.loglevel.default";
 	public static final String PINTAN_INIT_PARAM = "client.passport.PinTan.init";
 	public static final String PRODUCT_KEY_PARAM = "client.product.name";
+	public static final String PRODUCT_VERSION_PARAM = "client.product.version";
+	private static final int PRODUCT_VERSION_MAX_LENGTH = 5;
 
 	private HbciProperties() {
 	}
@@ -17,6 +20,15 @@ public final class HbciProperties {
 		Properties properties = new Properties();
 		properties.setProperty(PINTAN_INIT_PARAM, "1");
 		properties.setProperty(LOG_LEVEL_PARAM, Integer.toString(LoggingSettings.getHbciLogLevel().toHbciLogLevel()));
+		properties.setProperty(PRODUCT_VERSION_PARAM, toFinTsProductVersion(BuildInfo.getProgramVersion()));
 		return properties;
+	}
+
+	static String toFinTsProductVersion(String programVersion) {
+		int qualifierIndex = programVersion.indexOf('-');
+		int releaseVersionLength = qualifierIndex >= 0 ? qualifierIndex : programVersion.length();
+		int productVersionLength = Math.min(releaseVersionLength, PRODUCT_VERSION_MAX_LENGTH);
+		String productVersion = programVersion.substring(0, productVersionLength);
+		return productVersion.endsWith(".") ? productVersion.substring(0, productVersionLength - 1) : productVersion;
 	}
 }
--- src/test/java/de/zft2/gbanking/hbci/HbciPropertiesTest.java
+++ src/test/java/de/zft2/gbanking/hbci/HbciPropertiesTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import de.zft2.gbanking.db.BuildInfo;
 import de.zft2.gbanking.db.DBController;
 import de.zft2.gbanking.db.DBControllerTestUtil;
 import de.zft2.gbanking.db.dao.Setting;
@@ -45,5 +46,15 @@ class HbciPropertiesTest {
 
 		assertEquals("1", properties.getProperty(HbciProperties.PINTAN_INIT_PARAM));
 		assertEquals("5", properties.getProperty(HbciProperties.LOG_LEVEL_PARAM));
+		assertEquals(HbciProperties.toFinTsProductVersion(BuildInfo.getProgramVersion()),
+				properties.getProperty(HbciProperties.PRODUCT_VERSION_PARAM));
+	}
+
+	@Test
+	void shouldConvertApplicationVersionToFinTsProductVersion() {
+		assertEquals("0.3.0", HbciProperties.toFinTsProductVersion("0.3.0"));
+		assertEquals("0.3.1", HbciProperties.toFinTsProductVersion("0.3.1-SNAPSHOT"));
+		assertEquals("0.10", HbciProperties.toFinTsProductVersion("0.10.0"));
+		assertEquals("10.0", HbciProperties.toFinTsProductVersion("10.0.0"));
 	}
 }
